2a8f98268ea2da1eb1a7c7a03997a2775de015bf
Zurida <undinka08@mail.ru>
2024-06-12 22:12:43 +0300

    added spritesheet change on select

---
+++ index.js
@@ -0,0 +1,96 @@
+const canvas = document.querySelector('#canvas')
+const ctx = canvas.getContext('2d')
+
+const CANVAS_WIDTH = canvas.width = 600
+const CANVAS_HEIGHT = canvas.height = 600
+
+const playerImage = new Image();
+playerImage.src = 'img/shadow_dog.png';
+
+// spritesheet has width 6876px and 12 columns => 6876 / 12 = 573
+const spriteWidth = 575
+
+// spritesheet has height 5230 and 10 columns => 6876 / 12 = 523
+const spriteHeight = 523
+
+let playerState = 'run'
+const dropdown = document.getElementById('animations')
+dropdown.addEventListener('change', (e) => {
+    playerState = e.target.value
+})
+
+let gameFrame = 0
+const staggerFrames = 5
+const spriteAnimations = [] // container to hold all data for all animations
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7 // in spritesheet the first row with idle animation is made out of 7 frames 
+    },
+    {
+        name: 'jump',
+        frames: 7
+    },
+    {
+        name: 'fall',
+        frames: 7
+    },
+    {
+        name: 'run',
+        frames: 9
+    },
+    {
+        name: 'dizzy',
+        frames: 11
+    },
+    {
+        name: 'sit',
+        frames: 5
+    },
+    {
+        name: 'roll',
+        frames: 7
+    },
+    {
+        name: 'bite',
+        frames: 7
+    },
+    {
+        name: 'ko',
+        frames: 12
+    },
+    {
+        name: 'getHit',
+        frames: 4
+    }
+]
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: []
+    }
+
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth
+        let positionY = index * spriteHeight // location of sprite on spritesheet y axis 
+        frames.loc.push({ x: positionX, y: positionY })
+    }
+    spriteAnimations[state.name] = frames
+})
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // clear all paint between frames
+
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length
+    let frameX = spriteWidth * position
+    let frameY = spriteAnimations[playerState].loc[position].y
+
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    gameFrame++
+    requestAnimationFrame(animate);
+
+
+};
+
+
+animate();
